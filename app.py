52f90f9981c3b77408fe61d2783079bff5d00bb9
jjokah <johnjohnsonokah@gmai.com>
2022-08-19 10:44:40 +0100

    Deleting a Todo item

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ class Todo(db.Model):
         return f"<Todo {self.id} {self.description}>"
 
 
+# CREATE new item - and add to database #
 @app.route("/todos/create", methods=["POST"])
 def create_todo():
     body = {}
@@ -43,13 +44,12 @@ def create_todo():
             return jsonify(body)
 
 
+# UPDATE item complete status
 @app.route("/todos/<todo_id>/set-completed", methods=["POST"])
 def set_completed_todo(todo_id):
     try:
         completed = request.get_json()["completed"]
-        print("completed", completed)
         todo = Todo.query.get(todo_id)
-        print(todo_id)
         todo.completed = completed
         db.session.commit()
     except:
@@ -57,6 +57,19 @@ def set_completed_todo(todo_id):
     finally:
         db.session.close()
     return redirect(url_for("index"))
+
+
+# DELETE item from database
+@app.route("/todos/<todo_id>", methods=["DELETE"])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({"success": True})
 
 
 @app.route("/")
